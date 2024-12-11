5f0f919c62b68dcebf8754f7648fc381cc4df75c
kobjy002 <128272687+kobjy002@users.noreply.github.com>
2024-12-11 16:13:34 +1030

    Change runtime to use Arc instead of Box

--- jmespath/src/runtime.rs
+++ jmespath/src/runtime.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::sync::Arc;
+// use std::sync::RwLock;
 
 use crate::functions::*;
 use crate::parse;
@@ -11,13 +13,17 @@ use crate::JmespathError;
 /// You really only need to create your own Runtimes if you are
 /// utilizing custom functions in your expressions.
 pub struct Runtime {
-    functions: HashMap<String, Box<dyn Function>>,
+    // functions: HashMap<String, Box<dyn Function>>,
+    // functions: Arc<RwLock<HashMap<String, Box<dyn Function>>>>,
+    functions: HashMap<String, Arc<dyn Function>>,
+
 }
 
 impl Default for Runtime {
     fn default() -> Self {
         Runtime {
             functions: HashMap::with_capacity(26),
+            //functions: Arc::new(RwLock::new(HashMap::with_capacity(26))),
         }
     }
 }
@@ -39,50 +45,80 @@ impl Runtime {
 
     /// Adds a new function to the runtime.
     #[inline]
-    pub fn register_function(&mut self, name: &str, f: Box<dyn Function>) {
-        self.functions.insert(name.to_owned(), f);
+    pub fn register_function(&mut self, name: &str, f: Arc<dyn Function>) {
+        self.functions.insert(name.to_owned(), f.into());
+        // let mut functions = self.functions.write().unwrap();
+        // functions.insert(name.to_owned(), f);
     }
 
     /// Removes a function from the runtime.
     ///
     /// Returns the function that was removed if it was found.
-    pub fn deregister_function(&mut self, name: &str) -> Option<Box<dyn Function>> {
+    pub fn deregister_function(&mut self, name: &str) -> Option<Arc<dyn Function>> {
         self.functions.remove(name)
+        // let mut functions = self.functions.write().unwrap();
+        // functions.remove(name)
     }
 
     /// Gets a function by name from the runtime.
-    #[inline]
+    // #[inline]
     pub fn get_function<'a>(&'a self, name: &str) -> Option<&'a dyn Function> {
         self.functions.get(name).map(AsRef::as_ref)
     }
 
     /// Registers all of the builtin JMESPath functions with the runtime.
     pub fn register_builtin_functions(&mut self) {
-        self.register_function("abs", Box::new(AbsFn::new()));
-        self.register_function("avg", Box::new(AvgFn::new()));
-        self.register_function("ceil", Box::new(CeilFn::new()));
-        self.register_function("contains", Box::new(ContainsFn::new()));
-        self.register_function("ends_with", Box::new(EndsWithFn::new()));
-        self.register_function("floor", Box::new(FloorFn::new()));
-        self.register_function("join", Box::new(JoinFn::new()));
-        self.register_function("keys", Box::new(KeysFn::new()));
-        self.register_function("length", Box::new(LengthFn::new()));
-        self.register_function("map", Box::new(MapFn::new()));
-        self.register_function("min", Box::new(MinFn::new()));
-        self.register_function("max", Box::new(MaxFn::new()));
-        self.register_function("max_by", Box::new(MaxByFn::new()));
-        self.register_function("min_by", Box::new(MinByFn::new()));
-        self.register_function("merge", Box::new(MergeFn::new()));
-        self.register_function("not_null", Box::new(NotNullFn::new()));
-        self.register_function("reverse", Box::new(ReverseFn::new()));
-        self.register_function("sort", Box::new(SortFn::new()));
-        self.register_function("sort_by", Box::new(SortByFn::new()));
-        self.register_function("starts_with", Box::new(StartsWithFn::new()));
-        self.register_function("sum", Box::new(SumFn::new()));
-        self.register_function("to_array", Box::new(ToArrayFn::new()));
-        self.register_function("to_number", Box::new(ToNumberFn::new()));
-        self.register_function("to_string", Box::new(ToStringFn::new()));
-        self.register_function("type", Box::new(TypeFn::new()));
-        self.register_function("values", Box::new(ValuesFn::new()));
+        // self.register_function("abs", Box::new(AbsFn::new()));
+        // self.register_function("avg", Box::new(AvgFn::new()));
+        // self.register_function("ceil", Box::new(CeilFn::new()));
+        // self.register_function("contains", Box::new(ContainsFn::new()));
+        // self.register_function("ends_with", Box::new(EndsWithFn::new()));
+        // self.register_function("floor", Box::new(FloorFn::new()));
+        // self.register_function("join", Box::new(JoinFn::new()));
+        // self.register_function("keys", Box::new(KeysFn::new()));
+        // self.register_function("length", Box::new(LengthFn::new()));
+        // self.register_function("map", Box::new(MapFn::new()));
+        // self.register_function("min", Box::new(MinFn::new()));
+        // self.register_function("max", Box::new(MaxFn::new()));
+        // self.register_function("max_by", Box::new(MaxByFn::new()));
+        // self.register_function("min_by", Box::new(MinByFn::new()));
+        // self.register_function("merge", Box::new(MergeFn::new()));
+        // self.register_function("not_null", Box::new(NotNullFn::new()));
+        // self.register_function("reverse", Box::new(ReverseFn::new()));
+        // self.register_function("sort", Box::new(SortFn::new()));
+        // self.register_function("sort_by", Box::new(SortByFn::new()));
+        // self.register_function("starts_with", Box::new(StartsWithFn::new()));
+        // self.register_function("sum", Box::new(SumFn::new()));
+        // self.register_function("to_array", Box::new(ToArrayFn::new()));
+        // self.register_function("to_number", Box::new(ToNumberFn::new()));
+        // self.register_function("to_string", Box::new(ToStringFn::new()));
+        // self.register_function("type", Box::new(TypeFn::new()));
+        // self.register_function("values", Box::new(ValuesFn::new()));
+        self.register_function("abs", Arc::new(AbsFn::new()));
+        self.register_function("avg", Arc::new(AvgFn::new()));
+        self.register_function("ceil", Arc::new(CeilFn::new()));
+        self.register_function("contains", Arc::new(ContainsFn::new()));
+        self.register_function("ends_with", Arc::new(EndsWithFn::new()));
+        self.register_function("floor", Arc::new(FloorFn::new()));
+        self.register_function("join", Arc::new(JoinFn::new()));
+        self.register_function("keys", Arc::new(KeysFn::new()));
+        self.register_function("length", Arc::new(LengthFn::new()));
+        self.register_function("map", Arc::new(MapFn::new()));
+        self.register_function("min", Arc::new(MinFn::new()));
+        self.register_function("max", Arc::new(MaxFn::new()));
+        self.register_function("max_by", Arc::new(MaxByFn::new()));
+        self.register_function("min_by", Arc::new(MinByFn::new()));
+        self.register_function("merge", Arc::new(MergeFn::new()));
+        self.register_function("not_null", Arc::new(NotNullFn::new()));
+        self.register_function("reverse", Arc::new(ReverseFn::new()));
+        self.register_function("sort", Arc::new(SortFn::new()));
+        self.register_function("sort_by", Arc::new(SortByFn::new()));
+        self.register_function("starts_with", Arc::new(StartsWithFn::new()));
+        self.register_function("sum", Arc::new(SumFn::new()));
+        self.register_function("to_array", Arc::new(ToArrayFn::new()));
+        self.register_function("to_number", Arc::new(ToNumberFn::new()));
+        self.register_function("to_string", Arc::new(ToStringFn::new()));
+        self.register_function("type", Arc::new(TypeFn::new()));
+        self.register_function("values", Arc::new(ValuesFn::new()));
     }
 }
